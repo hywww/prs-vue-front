f2fc0bfc804c7975e5129b4695877d5f12d40ae0
heperwong <41944890+heperwong@users.noreply.github.com>
2019-08-28 11:37:24 +0800

    增加history

---
+++ router.js
@@ -0,0 +1,73 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import list from '@/views/list'
+import main from "@/views/main"
+import rightContent from './components/rightContent'
+import parameterIntro from './components/parameterIntro'
+import history from './components/history'
+import attention from './components/attention'
+import functionDemo from './components/functionDemo'
+import functionDemoDetail from './components/functionDemoDetail'
+import problemList from './components/problemList'
+import code from './components/code'
+
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      component: main,
+      children:[
+        {
+          path: 'content/:name',
+          name: 'detail',
+          component: rightContent
+        },
+        {
+          path: 'history/:name',
+          name: 'history',
+          component: history
+        },
+        {
+          path: 'parameter/:name',
+          name: 'parameter',
+          component: parameterIntro
+        },
+        {
+          path: 'attention/:name',
+          name: 'attention',
+          component: attention
+        },
+        {
+          path: 'function/edit',
+          name: 'functionDemoEdit',
+          component: functionDemo
+        },
+        {
+          path: 'function/detail/:name',
+          name: 'functionDemoDetail',
+          component: functionDemoDetail
+        },
+        {
+          path: 'problem/:name',
+          name: 'problemList',
+          component: problemList
+        },
+        {
+          path: 'code/:name/:type/:version',
+          name: 'code',
+          component: code
+        }
+      ]
+    },
+    {
+      path: '/list',
+      name: 'list',
+      component: list
+    },
+  ],
+  // mode: 'history',
+  // base: '/dist/'
+})
