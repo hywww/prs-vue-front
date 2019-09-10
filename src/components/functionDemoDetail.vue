6a464e649b8c949679ae8a0acc97fcc58b221e68
heperwong <41944890+heperwong@users.noreply.github.com>
2019-09-10 22:07:17 +0800

    修改pdf浏览，调用数据库

--- src/components/functionDemoDetail.vue
+++ src/components/functionDemoDetail.vue
@@ -1,45 +1,66 @@
 <template>
-    <div class="content">
-        <router-link class="return_prev" :to="{name:'detail', params:{name:sourceData.macroName}}">返回上一页》</router-link>
-        <iframe src="http://pv33lxmbm.bkt.clouddn.com/dsoutput.pdf" :style="styles" class="iframe-pdf"></iframe>
-    </div>
+    <section class="functionDemoDetail">
+        <router-link class="return_prev" :to="{name:'detail', params:{name:macroName}}">返回上一页》</router-link>
+        <div></div>
+        <iframe :src="pa"  class="iframe-pdf"></iframe>
+    </section>
 </template>
 <style scoped>
-.content{
-    width: 1020px;
-}
 .return_prev{
     font-size: 12px;
 }
 .iframe-pdf {
-  margin-top: 20px;
-  width: 100%;
+  margin-top: 5px;
+  width: 81%;
+  height: 600px;
 }
 </style>
 <script>
 export default {
-    name:'functionDemoDetail',
     data(){
         return{
-            sourceData: {
-              macroName: this.$route.params.name
-            },
-            baseUrl: process.env.BASE_URL,
-            styles: {
-              height: '500px'
-            },         
+            parameterList_: [],
+            parameterKeys: [],
+            macroName: this.$route.params.name,
+            lpath: this.$route.params.lpath,
+            pa: [],
         }
     },
     watch: {
         $route: function(mew, old){
-            // this.getData();
+            this.getData();
         }
     },
     created(){
-      this.styles.height = window.screen.height - 240 + 'px';
-        // this.getData();
+        this.getData();
     },
     methods: {
+        getData : function(){
+            let _that = this;
+            // AJAX.default.ajaxGet(this.baseUrl+'data/'+this.$route.params.id+'.json',function(result){
+            //     if(result.status == 200){
+            //         _that.sourceData = {...result.data};
+            //     }
+            // })
+            AJAX.default.ajaxGet(API.default.getLink,
+                function(result){
+                    let data = result.data;
+                    if(data.root){
+                        _that.parameterList_ = {...data.result};
+                        _that.parameterKeys = data.result[0] && Object.keys(data.result[0]);
+                        _that.pa = data.result[0].lpath;
+                        console.log(data.result[0].lpath)
+                    }
+                },
+                {
+                    name: _that.$route.params.name,
+                    lpath: _that.$route.params.lpath
+                }
+            )
+        },
     },
+    name: 'functionDemoDetail'
 }
 </script>
+
+
